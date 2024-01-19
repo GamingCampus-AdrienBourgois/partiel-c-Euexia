345343947d70726555d1057cc3cd2230157712b5
unknown <lquintana@gamingcampus.local>
2024-01-19 13:57:27 +0100

    jenaimarre

--- StudentLibrary/Solution2.cpp
+++ StudentLibrary/Solution2.cpp
@@ -1,5 +1,6 @@
 #include "Solution2.h"
-
+#include <fstream>
+#include <iostream>
 #include <ostream>
 
 // Don't forget to enable the exercise in the SudentConfiguration.h file !
@@ -8,7 +9,35 @@
 
 float Solution2::GetBalance(const std::string& accountName)
 {
-	return -1.0f;
+	std::string filePath = "BankAccount/" + accountName;
+
+	std::ifstream input(filePath);
+
+	if (!input.is_open()) {
+		throw std::runtime_error("File not found" + filePath);}
+
+
+    float balance = 0.0f;
+
+		char transaction;
+		float amount;
+
+
+        while (input >> transaction >> amount)
+        {
+            // Update balance based on transaction type
+            if (transaction == '+')
+            {
+                balance += amount; // Deposit
+        }
+            else if (transaction == '-')
+            {
+                balance -= amount; // Withdrawal
+            }
+    }
+
+        input.close();
+	    return -1.0f;
 }
 
 #endif
