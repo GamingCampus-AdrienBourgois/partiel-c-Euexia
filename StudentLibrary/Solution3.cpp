03827e0412d129eafb5a34e48649ff06be85551b
unknown <lquintana@gamingcampus.local>
2024-01-19 14:48:48 +0100

    cestuncomplot

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -1,25 +1,26 @@
 #include "Solution3.h"
+#include "Solution3.h"
 #include <algorithm>
 
-
-// Don't fortget to enable the exercise in the SudentConfiguration.h file !
-#include "StudentConfiguration.h"
 #ifdef COMPILE_EXERCICE_3
 
 void Solution3::SetWords(const std::vector<std::string>& _words)
 {
-	words = _words;
+    words = _words;
 }
 
-void Solution3::SortWords() {}
-
-std::vector<std::string> Solution3::GetSortedWords() const
+void Solution3::SortWords()
 {
     std::sort(words.begin(), words.end(), [](const std::string& a, const std::string& b) {
         return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end(), [](char c1, char c2) {
             return tolower(c1) < tolower(c2);
             });
         });
+}
+
+std::vector<std::string> Solution3::GetSortedWords() const
+{
+    return words;
 }
 
 #endif
