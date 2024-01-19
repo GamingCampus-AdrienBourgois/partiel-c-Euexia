25186f978d6642dbf542728a53f2c343d8198359
unknown <lquintana@gamingcampus.local>
2024-01-19 13:24:30 +0100

    test

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -6,7 +6,45 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
+    if (_from == _to) {
+ 
+        return _value;
+    }
+
+    switch (_from)
+    {
+    case TemperatureUnits::CELSIUS:
+        switch (_to)
+        {
+        case TemperatureUnits::FAHRENHEIT:
+            return (_value * 9 / 5) + 32;
+        case TemperatureUnits::KELVIN:
+            return _value + 273.15f;
+        }
+        break;
+
+    case TemperatureUnits::FAHRENHEIT:
+        switch (_to)
+        {
+        case TemperatureUnits::CELSIUS:
+            return (_value - 32) * 5 / 9;
+        case TemperatureUnits::KELVIN:
+            return (_value - 32) * 5 / 9 + 273.15f;
+        }
+        break;
+
+    case TemperatureUnits::KELVIN:
+        switch (_to)
+        {
+        case TemperatureUnits::CELSIUS:
+            return _value - 273.15f;
+        case TemperatureUnits::FAHRENHEIT:
+            return (_value - 273.15f) * 9 / 5 + 32;
+        }
+        break;
+    }
+
+    return -1.0f;
 }
 
 #endif
